00028b8e928fb1466b7bc570c2521655963086ae
Jose <mondradev@gmail.com>
2024-05-21 17:56:39 -0400

    adding lost docs and fixing grammar 0521241756

--- src/Home.js
+++ src/Home.js
@@ -5,7 +5,7 @@ import FeedbackForm from "./FeedbackForm";
 class Home extends Component {
     render() {
         return(
-            <div><p>Looking for a data science or math tool for your class? We have you covered!</p>
+            <div><p>Looking for a data science or math tool for your class? I have you covered!</p>
             <section className="feedback-section">
             <FeedbackForm />
             </section>
