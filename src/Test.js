c78e83bcde45c84062619e1cd6e23a69bf1bfa69
Jose Mondragon <121974632+mondragon-developer@users.noreply.github.com>
2024-06-13 17:55:56 -0400

    test link temporary fixed

--- src/Test.js
+++ src/Test.js
@@ -2,29 +2,47 @@ import React, { Component } from "react";
 import UserForm from './UserForm';
 import TestForm from './TestForm';
 
-
 class Test extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          currentUser: null,
-        };
-      }
-    
-      handleUserCreated = (user) => {
-        this.setState({ currentUser: user });
-      };
-    render() {
-        const { currentUser } = this.state;
-        return(
-            <div>
-            {!this.state.currentUser ? (
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentUser: null,
+    };
+  }
+
+  handleUserCreated = (user) => {
+    this.setState({ currentUser: user });
+  };
+
+  // componentDidMount() {
+  //   const script = document.createElement("script");
+  //   script.type = "text/javascript";
+  //   script.language = "JavaScript";
+  //   script.src = "https://na1.documents.adobe.com/public/embeddedWidget?wid=CBFCIBAA3AAABLblqZhBG3XmRNEh7Fp4N_CsU0MPztUMM8PR0ehkvMsX5EC9XuJwvHbJpOBDDvTqM9bpiD60*";
+  //   script.async = true;
+  //   document.getElementById("adobe-widget-container").appendChild(script);
+  // }
+
+  render() {
+    const { currentUser } = this.state;
+    const testLink = "https://na1.documents.adobe.com/public/esignWidget?wid=CBFCIBAA3AAABLblqZhBG3XmRNEh7Fp4N_CsU0MPztUMM8PR0ehkvMsX5EC9XuJwvHbJpOBDDvTqM9bpiD60*";
+
+    return (
+      <div>
+        {/* {!currentUser ? (
           <UserForm onUserCreated={this.handleUserCreated} />
         ) : (
           <TestForm userId={currentUser._id} />
-        )}
-            </div>
-        );
-    }
+        )} */}
+        {/* <div id="adobe-widget-container"></div> */}
+        <div className="link-container">
+          <a href={testLink} className="test-link" target="_blank" rel="noopener noreferrer">
+            Submit Answers of your test
+          </a>
+        </div>
+      </div>
+    );
+  }
 }
+
 export default Test;
